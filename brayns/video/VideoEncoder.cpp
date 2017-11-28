1e8d1e275b2779998857b4ff675dcb57d6d7f752
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2017-11-28 14:07:59 +0100

    tweaks

--- brayns/video/VideoEncoder.cpp
+++ brayns/video/VideoEncoder.cpp
@@ -47,28 +47,33 @@ public:
         av_register_all();
         avformat_network_init();
 
-        const AVCodecID codec_id = AV_CODEC_ID_H264;
-        const int width = 1920;
-        const int height = 1080;
+        // av_log_set_level(AV_LOG_DEBUG);
 
-        codec = avcodec_find_encoder(codec_id);
+        codec = avcodec_find_encoder(codecID);
         context = avcodec_alloc_context3(codec);
 
-        context->bit_rate = 400000;
+        context->bit_rate = bitrate;
         context->width = width;
         context->height = height;
         context->time_base.num = 1;
-        context->time_base.den = 25;
-        context->gop_size = 25;
+        context->time_base.den = fps;
+        context->gop_size = 48;
         context->max_b_frames = 1;
-        context->pix_fmt = AV_PIX_FMT_YUV420P;
+        context->pix_fmt = pixfmt;
         context->codec_type = AVMEDIA_TYPE_VIDEO;
-        // context->flags = CODEC_FLAG_GLOBAL_HEADER;
+        // context->flags |= CODEC_FLAG_GLOBAL_HEADER;
 
-        if (codec_id == AV_CODEC_ID_H264)
+        if (codecID == AV_CODEC_ID_H264)
         {
+            av_opt_set(context->priv_data, "profile", "baseline", 0);
+            // av_opt_set(context->priv_data, "level", "32", 0);
+            av_opt_set(context->priv_data, "intra-refresh", "1", 0);
+            // av_opt_set(context->priv_data, "crf", "15", 0);
             av_opt_set(context->priv_data, "preset", "ultrafast", 0);
             av_opt_set(context->priv_data, "tune", "zerolatency", 0);
+            // av_opt_set(context->priv_data, "slice-max-size", "1500", 0);
+            av_opt_set(context->priv_data, "slices", "4", 0);
+            // av_opt_set(context->priv_data, "threads", "4", 0);
         }
 
         avcodec_open2(context, codec, NULL);
@@ -78,7 +83,11 @@ public:
         frame->width = context->width;
         frame->height = context->height;
         av_image_alloc(frame->data, frame->linesize, context->width,
-                       context->height, context->pix_fmt, 32);
+                       context->height, context->pix_fmt, 24);
+
+        av_init_packet(&pkt);
+        pkt.data = nullptr; // packet data will be allocated by the encoder
+        pkt.size = 0;
 
         _setupStream();
     }
@@ -126,49 +135,19 @@ public:
         if (!rgba)
             throw std::invalid_argument("what the fuck");
 
-// we could use threads here for sendframe and receivepacket, but hey...
-#ifdef FFMPEG_3_1
-        AVPacket* pkt = av_packet_alloc();
-        int ret;
-
-        ret = avcodec_send_frame(context, frame);
-        if (ret >= 0)
-        {
-            while (!ret)
-            {
-                ret = avcodec_receive_packet(context, pkt);
-                if (!ret)
-                    ret = cb(priv, pkt);
-            }
-        }
-
-        av_packet_free(&pkt);
-//        if (ret == AVERROR(EAGAIN))
-//            return 0;
-//        return ret;
-#else
-        AVPacket pkt;
-        av_init_packet(&pkt);
-        pkt.data = NULL; // packet data will be allocated by the encoder
-        pkt.size = 0;
-        fflush(stdout);
-
         // rgba to yuv
         {
             int in_linesize[1] = {4 * context->width};
-            sws_context = sws_getCachedContext(sws_context, context->width,
-                                               context->height, AV_PIX_FMT_RGBA,
-                                               context->width, context->height,
-                                               AV_PIX_FMT_YUV420P, 0, 0, 0, 0);
+            sws_context =
+                sws_getCachedContext(sws_context, context->width,
+                                     context->height, AV_PIX_FMT_RGBA,
+                                     context->width, context->height, pixfmt,
+                                     SWS_FAST_BILINEAR, 0, 0, 0);
 
             sws_scale(sws_context, (const uint8_t* const*)&rgba, in_linesize, 0,
                       context->height, frame->data, frame->linesize);
         }
         frame->pts = currentFrame++;
-
-        /* encode the image */
-        // avcodec_send_frame(context, frame);
-        // avcodec_receive_packet(context, &pkt);
 
         int got_output = false;
         int ret = avcodec_encode_video2(context, &pkt, frame, &got_output);
@@ -177,60 +156,92 @@ public:
             std::cerr << "video encode error " << ret << std::endl;
             return;
         }
-
-        //        if (ret == AVERROR_EOF) {
-        //            got_output = false;
-        //            printf("Stream EOF\n");
-        //        } else if(ret == AVERROR(EAGAIN)) {
-        //            got_output = false;
-        //            printf("Stream EAGAIN\n");
-        //        } else {
-        //            got_output = true;
-        //        }
-
         if (got_output)
         {
             // printf("Write frame %3d (size=%5d)\n", outputFrame++, pkt.size);
+
+            /* rescale output packet timestamp values from codec to stream
+             * timebase */
+            //            pkt.pts = av_rescale_q_rnd(pkt.pts,
+            //            context->time_base, stream->time_base,
+            //            AVRounding(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+            //            pkt.dts = av_rescale_q_rnd(pkt.dts,
+            //            context->time_base, stream->time_base,
+            //            AVRounding(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+            //            pkt.duration = av_rescale_q(pkt.duration,
+            //            context->time_base, stream->time_base);
+            pkt.stream_index = stream->index;
+
             av_interleaved_write_frame(avfctx, &pkt);
+
             av_packet_unref(&pkt);
         }
-#endif
     }
 
 private:
     AVCodec* codec;
     AVCodecContext* context;
     AVFrame* frame;
+    AVPacket pkt;
     AVFormatContext* avfctx;
     size_t currentFrame{0};
     // size_t outputFrame{0};
     SwsContext* sws_context{nullptr};
+    AVStream* stream{nullptr};
+    AVPixelFormat pixfmt{AV_PIX_FMT_YUV420P};
+    int bitrate{3000000};
+    AVCodecID codecID{AV_CODEC_ID_H264};
+    const int width = 1920;
+    const int height = 1080;
+    const int fps = 30;
 
     void _setupStream()
     {
+#define RTP
+#ifdef RTP
         AVOutputFormat* fmt = av_guess_format("rtp", NULL, NULL);
-
-        /*int ret = */ avformat_alloc_output_context2(&avfctx, fmt, "h264",
-                                                      "rtp://127.0.0.1:49990");
+        const char* url = "rtp://233.233.233.233:6666";
+        const char* fmt_bla = "h264";
+#else
+        AVOutputFormat* fmt = nullptr;
+        const char* url = "rtsp://127.0.0.1:49990/video/0";
+        const char* fmt_bla = "rtsp";
+#endif
+        /*int ret = */ avformat_alloc_output_context2(&avfctx, fmt, fmt_bla,
+                                                      url);
 
         printf("Writing to %s\n", avfctx->filename);
 
         avio_open(&avfctx->pb, avfctx->filename, AVIO_FLAG_WRITE);
 
-        // avfctx->bit_rate = 400000;
+        avfctx->bit_rate = bitrate;
         // avfctx->video_codec_id = AV_CODEC_ID_H264;
         // avfctx->video_codec
 
-        struct AVStream* stream = avformat_new_stream(avfctx, codec);
-        stream->codec->bit_rate = 400000;
+        stream = avformat_new_stream(avfctx, codec);
+        stream->codec->bit_rate = bitrate;
         stream->codec->width = context->width;
         stream->codec->height = context->height;
-        //        stream->codecpar->codec_id = AV_CODEC_ID_H264;
+        stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
+        stream->codec->codec_id = codecID;
         //        stream->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
-        stream->time_base.num = 1;
-        stream->time_base.den = 25;
+        //        stream->time_base.num = 1;
+        //        stream->time_base.den = 25;
 
-        avformat_write_header(avfctx, NULL);
+        stream->id = avfctx->nb_streams - 1;
+        stream->time_base.den = fps;
+        stream->time_base.num = 1;
+
+        av_dump_format(avfctx, 0, url, 1);
+
+        int ret = avformat_write_header(avfctx, NULL);
+        if (ret != 0)
+        {
+            av_log(NULL, AV_LOG_ERROR,
+                   "Failed to connect to RTSP server for '%s'.\n", url);
+            return;
+        }
+
         char buf[200000];
         AVFormatContext* ac[] = {avfctx};
         av_sdp_create(ac, 1, buf, 20000);

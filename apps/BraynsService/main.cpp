df2886e9a93f4d6c0d869e0b79ad8bb530fa8381
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2018-02-01 15:41:14 +0100

    uv test

--- apps/BraynsService/main.cpp
+++ apps/BraynsService/main.cpp
@@ -23,26 +23,118 @@
 #include <brayns/common/log.h>
 #include <brayns/common/types.h>
 
-int main(int argc, const char** argv)
-{
-    try
-    {
-        BRAYNS_INFO << "Initializing Service..." << std::endl;
-        brayns::Brayns brayns(argc, argv);
+#include <thread>
+#include <uv.h>
 
-        brayns::Timer timer;
-        timer.start();
-        bool keepRunning = true;
-        while (keepRunning)
-            keepRunning = brayns.render();
-        timer.stop();
-        BRAYNS_INFO << "Service was running for " << timer.seconds()
-                    << " seconds" << std::endl;
-    }
-    catch (const std::runtime_error& e)
-    {
-        BRAYNS_ERROR << e.what() << std::endl;
-        return 1;
-    }
+size_t nTimes = 2;
+
+void timer_callback(uv_timer_t *handle)
+{
+    uv_async_t *other_thread_notifier = (uv_async_t *)handle->data;
+
+    fprintf(stderr, "Timer expired, notifying other thread\n");
+
+    if (--nTimes == 0)
+        uv_stop(uv_default_loop());
+
+    // Notify the other thread
+    uv_async_send(other_thread_notifier);
+}
+
+void render_loop(uv_loop_t *thread_loop)
+{
+    fprintf(stderr, "Consumer thread will start event loop\n");
+
+    // Start this loop
+    uv_run(thread_loop, UV_RUN_DEFAULT);
+}
+
+void consumer_notify(uv_async_t *handle)
+{
+    fprintf(stderr, "Hello from the other thread\n", handle->loop->backend_fd);
+    if (nTimes == 0)
+        uv_stop(handle->loop);
+}
+
+int main(int argc, char *argv[])
+{
+    uv_async_t async;
+
+    /* Create and set up the consumer thread */
+    uv_loop_t *thread_loop = uv_loop_new();
+    uv_async_init(thread_loop, &async, consumer_notify);
+    std::thread render_thread(std::bind(&render_loop, thread_loop));
+
+    /* Main thread will run default loop */
+    uv_loop_t *main_loop = uv_default_loop();
+    uv_timer_t timer_req;
+    uv_timer_init(main_loop, &timer_req);
+
+    /* Timer callback needs async so it knows where to send messages */
+    timer_req.data = &async;
+    uv_timer_start(&timer_req, timer_callback, 0, 500);
+
+    fprintf(stderr, "Starting main loop\n");
+    uv_run(main_loop, UV_RUN_DEFAULT);
+
+    render_thread.join();
+
     return 0;
 }
+
+// struct Work
+//{
+//    size_t n{42};
+//};
+
+// void do_work(uv_work_t *req) {
+//    Work n = *(Work *) req->data;
+//    std::cout << std::this_thread::get_id() << std::endl;
+//    fprintf(stderr, "%dth fibonacci\n", n.n);
+//}
+
+// void work_cb(uv_work_t *req, int status) {
+//    fprintf(stderr, "Done calculating %dth fibonacci\n", *(int *) req->data);
+//}
+
+// int main(int argc, const char** argv)
+//{
+////    try
+////    {
+////        BRAYNS_INFO << "Initializing Service..." << std::endl;
+////        brayns::Brayns brayns(argc, argv);
+
+////        brayns::Timer timer;
+////        timer.start();
+////        bool keepRunning = true;
+////        while (keepRunning)
+////            keepRunning = brayns.render();
+////        timer.stop();
+////        BRAYNS_INFO << "Service was running for " << timer.seconds()
+////                    << " seconds" << std::endl;
+////    }
+////    catch (const std::runtime_error& e)
+////    {
+////        BRAYNS_ERROR << e.what() << std::endl;
+////        return 1;
+////    }
+////    return 0;
+
+//    uv_loop_t *loop = (uv_loop_t *)malloc(sizeof(uv_loop_t));
+//    uv_loop_init(loop);
+
+//    uv_work_t req, req1, req2;
+//    Work work;
+//    req.data = (void *) &work;
+//    req1.data = (void *) &work;
+//    req2.data = (void *) &work;
+//    uv_queue_work(loop, &req, do_work, work_cb);
+//    uv_queue_work(loop, &req1, do_work, work_cb);
+//    uv_queue_work(loop, &req2, do_work, work_cb);
+
+//    uv_run(loop, UV_RUN_DEFAULT);
+
+//    uv_loop_close(loop);
+//    free(loop);
+//    return 0;
+//}

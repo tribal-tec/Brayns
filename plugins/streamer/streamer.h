9d487d5e7d9ceed91ee5a0084a4275c12d7ebbf6
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2019-08-15 15:13:45 +0200

    No more Lunchbox; MPI and NvPipe required, no more CPU encoding

--- plugins/streamer/streamer.h
+++ plugins/streamer/streamer.h
@@ -1,63 +1,44 @@
+/* Copyright (c) 2019, EPFL/Blue Brain Project
+ *
+ * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #pragma once
 
 extern "C" {
-#include <libavcodec/avcodec.h>
-#include <libavutil/channel_layout.h>
-#include <libavutil/common.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/opt.h>
-#include <libavutil/samplefmt.h>
-
-#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#include <libavutil/imgutils.h>
-#include <libswscale/swscale.h>
 }
 
 #include <atomic>
+#include <condition_variable>
+#include <queue>
 #include <string>
+#include <thread>
 
 #include <brayns/common/PropertyMap.h>
 #include <brayns/common/Timer.h>
 #include <brayns/common/types.h>
 #include <brayns/pluginapi/ExtensionPlugin.h>
 
-#include <lunchbox/monitor.h>
-#include <lunchbox/mtQueue.h>
-#include <thread>
-
-#ifdef USE_MPI
 #include <ospray/mpiCommon/MPIBcastFabric.h>
-#endif
 
-#ifdef USE_NVPIPE
 #include <NvPipe.h>
-#endif
 
 namespace streamer
 {
-class Picture
-{
-public:
-    AVFrame *frame{nullptr};
-
-    int init(enum AVPixelFormat pix_fmt, int width, int height)
-    {
-        frame = av_frame_alloc();
-        frame->format = pix_fmt;
-        frame->width = width;
-        frame->height = height;
-        return av_frame_get_buffer(frame, 32);
-    }
-
-    ~Picture()
-    {
-        if (frame)
-            av_frame_free(&frame);
-    }
-};
-
 struct Image
 {
     Image() = default;
@@ -70,6 +51,53 @@ struct Image
     const brayns::Vector2ui size;
     std::vector<char> data;
     const void *buffer{nullptr};
+};
+
+template <typename T, size_t S = 2>
+class MTQueue
+{
+public:
+    explicit MTQueue(const size_t maxSize = S)
+        : _maxSize(maxSize)
+    {
+    }
+
+    void push(const T &element)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _condition.wait(lock, [&] { return _queue.size() < _maxSize; });
+        _queue.push(element);
+        _condition.notify_all();
+    }
+
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _condition.wait(lock, [&] { return !_queue.empty(); });
+
+        T element = _queue.front();
+        _queue.pop();
+        _condition.notify_all();
+        return element;
+    }
+    size_t size() const
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        return _queue.size();
+    }
+
+    size_t waitSize(const size_t minSize) const
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _condition.wait(lock, [&] { return _queue.size() >= minSize; });
+        return _queue.size();
+    }
+
+private:
+    std::queue<T> _queue;
+    mutable std::mutex _mutex;
+    mutable std::condition_variable _condition;
+    const size_t _maxSize;
 };
 
 class Streamer : public brayns::ExtensionPlugin
@@ -92,17 +120,15 @@ private:
     {
         return _props.getProperty<bool>("async-encode");
     }
-    auto asyncCopy() const { return _props.getProperty<bool>("async-copy"); }
-    bool useGPU() const;
-    bool useMPI() const;
+    auto useMPI() const { return _props.getProperty<bool>("mpi"); }
     bool useCudaBuffer() const;
     bool isLocalOrMaster() const;
 
+    void _initMPI();
     void _runAsyncEncode();
-    void _runAsyncEncodeFinish();
     void encodeFrame(const size_t frameNumber, const brayns::Vector2ui &size,
                      const void *data);
-    void streamFrame(const bool finishEncode = true);
+    void streamFrame();
     void _syncFrame();
     void _barrier();
     void _nextFrame();
@@ -114,33 +140,24 @@ private:
     AVFormatContext *streamContext{nullptr};
     AVStream *stream{nullptr};
 
-    AVCodecContext *codecContext{nullptr};
     AVCodec *codec{nullptr};
 
     AVPacket *pkt{nullptr};
-    SwsContext *sws_context{nullptr};
-    Picture picture;
 
     brayns::Timer _timer;
     int64_t _waitTime{0};
 
     std::thread _encodeThread;
-    std::thread _encodeFinishThread; // CPU only
-    lunchbox::MTQueue<Image> _images;
-    lunchbox::Monitor<int> _pkts;
+    MTQueue<Image> _images;
 
-#ifdef USE_NVPIPE
     NvPipe *encoder{nullptr};
-#endif
     bool _fbModified{false};
     brayns::FrameBufferPtr frameBuffers[2];
-    int currentFB{0};
 
     const brayns::PropertyMap _props;
     size_t _frameNumber{0};
     std::atomic<double> encodeDuration{0};
-#ifdef USE_MPI
-    void _initMPI();
+
     double mpiDuration{0};
     double barrierDuration{0};
 
@@ -159,6 +176,5 @@ private:
         brayns::Renderer &renderer;
     };
     std::unique_ptr<FrameData> _frameData;
-#endif
 };
 }
